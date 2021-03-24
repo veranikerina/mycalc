cd28b7e164ec375173af96601c0e8dc1a004605e
Vera Nikerina <nikerina.vera@gmail.com>
2021-03-24 21:34:08 +0300

    my calc без вывода

---
+++ mycalc.py
@@ -0,0 +1,62 @@
+example = input ("Please type command: ")
+#9 - 2 ^ 3 - 1 + 8 / 4 * 3 ^ 2
+example
+import re
+splited_line = re.split(' ', example)
+splited_line
+for i,x in enumerate(splited_line):
+    if x == '^':
+        a = int (splited_line[i-1]) 
+        b = int (splited_line[i+1])
+        c = a ** b
+
+        splited_line.pop(i-1)
+        splited_line.pop(i-1)
+        splited_line.pop(i-1)
+        splited_line.insert(i-1,c)
+
+for i,x in enumerate(splited_line):
+    if x == '/':
+        a = int (splited_line[i-1])
+        b = int (splited_line[i+1])
+        c = a / b
+
+        splited_line.pop(i-1)
+        splited_line.pop(i-1)
+        splited_line.pop(i-1)
+        splited_line.insert(i-1,c)
+
+for i,x in enumerate(splited_line):
+    if x == '*':
+        a = int (splited_line[i-1])
+        b = int (splited_line[i+1])
+        c = a * b
+
+        splited_line.pop(i-1)
+        splited_line.pop(i-1)
+        splited_line.pop(i-1)
+        splited_line.insert(i-1,c)
+
+
+for i,x in enumerate(splited_line):
+    if x == '+':
+        a = int (splited_line[i-1])
+        b = int (splited_line[i+1])
+        c = a + b
+
+        splited_line.pop(i-1)
+        splited_line.pop(i-1)
+        splited_line.pop(i-1)
+        splited_line.insert(i-1,c)
+
+for i,x in enumerate(splited_line):
+    if x == '-':
+        a = int (splited_line[i-1])
+        b = int (splited_line[i+1])
+        c = a - b
+
+        splited_line.pop(i-1)
+        splited_line.pop(i-1)
+        splited_line.pop(i-1)
+        splited_line.insert(i-1,c)
+        print("Result " + str(c))

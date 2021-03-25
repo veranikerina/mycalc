b8a4418f3531aba14966697e2c8a9415008eb17e
Vera Nikerina <nikerina.vera@gmail.com>
2021-03-25 23:04:37 +0300

    калькулятор с приоритетом

--- mycalc.py
+++ mycalc.py
@@ -1,4 +1,4 @@
-example = input ("Please type command: ")
+example = input ("Please type command with space: ")
 #9 - 2 ^ 3 - 1 + 8 / 4 * 3 ^ 2
 example
 import re
@@ -9,62 +9,34 @@ for i,x in enumerate(splited_line):
         a = int (splited_line[i-1]) 
         b = int (splited_line[i+1])
         c = a ** b
-        print(a, b, c)
 
         splited_line.pop(i-1)
         splited_line.pop(i-1)
         splited_line.pop(i-1)
         splited_line.insert(i-1,c)
-        print(splited_line)
 
-for i,x in enumerate(splited_line):
-    if x == '/':
-        a = int (splited_line[i-1])
-        b = int (splited_line[i+1])
-        c = a / b
-        print(a, b, c)
+for i, x in enumerate(splited_line):
+    if x == '*' or x == '/':
+        a = int(splited_line[i - 1])
+        b = int(splited_line[i + 1])
+        if (splited_line[i]) == '*':
+            c = a * b
+        else:
+            c = a / b
 
-        splited_line.pop(i-1)
-        splited_line.pop(i-1)
-        splited_line.pop(i-1)
-        splited_line.insert(i-1,c)
-        print(splited_line)
+        splited_line.pop(i - 1)
+        splited_line.pop(i - 1)
+        splited_line.pop(i - 1)
+        splited_line.insert(i - 1, c)
 
-for i,x in enumerate(splited_line):
-    if x == '*':
-        a = int (splited_line[i-1])
-        b = int (splited_line[i+1])
-        c = a * b
-        print(a, b, c)
+c = int(splited_line[0])
+for i, x in enumerate(splited_line):
+    if x == '+' or x == '-':
+        # a = int (splited_line[i-1])
+        b = int(splited_line[i + 1])
+        if (splited_line[i]) == '+':
+            c = c + b
+        else:
+            c = c - b
 
-        splited_line.pop(i-1)
-        splited_line.pop(i-1)
-        splited_line.pop(i-1)
-        splited_line.insert(i-1,c)
-        print(splited_line)
-
-for i,x in enumerate(splited_line):
-    if x == '+':
-        a = int (splited_line[i-1])
-        b = int (splited_line[i+1])
-        c = a + b
-        print(a, b, c)
-
-        splited_line.pop(i-1)
-        splited_line.pop(i-1)
-        splited_line.pop(i-1)
-        splited_line.insert(i-1,c)
-        print(splited_line)
-
-for i,x in enumerate(splited_line):
-    if x == '-':
-        a = int (splited_line[i-1])
-        b = int (splited_line[i+1])
-        c = a - b
-        print(a, b, c)
-
-        splited_line.pop(i-1)
-        splited_line.pop(i-1)
-        splited_line.pop(i-1)
-        splited_line.insert(i-1,c)
-        print("Result " + str(c))
+print(str(c))
